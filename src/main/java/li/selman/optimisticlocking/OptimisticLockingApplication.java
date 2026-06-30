55cca04fad503a152d999eebdac0205771ca94d2
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2026-06-30 22:29:28 +0200

    Init

---
+++ src/main/java/li/selman/optimisticlocking/OptimisticLockingApplication.java
@@ -0,0 +1,13 @@
+package li.selman.optimisticlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimisticLockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimisticLockingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/li/selman/optimisticlocking/OptimisticLockingApplicationTests.java
@@ -0,0 +1,13 @@
+package li.selman.optimisticlocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimisticLockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
