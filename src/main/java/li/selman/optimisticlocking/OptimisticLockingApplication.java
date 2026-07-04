fddc2811797d89736fc526471bfdc5e9d71fa2de
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2026-07-04 10:15:30 +0200

    Use stable Pageable DTO

--- src/main/java/li/selman/optimisticlocking/OptimisticLockingApplication.java
+++ src/main/java/li/selman/optimisticlocking/OptimisticLockingApplication.java
@@ -2,8 +2,13 @@ package li.selman.optimisticlocking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+// For stable Pageable DTO
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class OptimisticLockingApplication {
 
     public static void main(String[] args) {
