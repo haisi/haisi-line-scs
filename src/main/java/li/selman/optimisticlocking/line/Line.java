c54e123b8793143f5668b78cc8ef50e94e13f55e
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2026-07-04 10:10:58 +0200

    Set lock-version to 1 on creation

--- src/main/java/li/selman/optimisticlocking/line/Line.java
+++ src/main/java/li/selman/optimisticlocking/line/Line.java
@@ -16,7 +16,7 @@ public class Line implements AggregateRoot<Line, LineId> {
     private LineId id;
 
     @Version
-    private long lockVersion;
+    private long lockVersion = 1;
 
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "left_id")
