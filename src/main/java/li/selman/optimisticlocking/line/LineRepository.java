3ea7c0e322e137343f31d9dc6ac46159689abdc8
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2026-07-04 09:14:40 +0200

    Add pageable findAll repository method

--- src/main/java/li/selman/optimisticlocking/line/LineRepository.java
+++ src/main/java/li/selman/optimisticlocking/line/LineRepository.java
@@ -1,6 +1,8 @@
 package li.selman.optimisticlocking.line;
 
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
@@ -15,6 +17,8 @@ public interface LineRepository extends Repository<Line, LineId> {
     Optional<Line> findById(LineId id);
 
     void delete(Line line);
+
+    Page<Line> findAll(Pageable pageable);
 
     /**
      * Loads for a mutation that only touches a child (Left/RightPoint): forces the root's
