a0b41b2b45918d492fbe3806d40e5e22b6c8f887
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2026-07-04 09:56:30 +0200

    Add static 'of' factory to identifier
    This way jmolecules spring-integration automatically registers a converter

--- src/main/java/li/selman/optimisticlocking/line/LineId.java
+++ src/main/java/li/selman/optimisticlocking/line/LineId.java
@@ -7,4 +7,13 @@ import java.util.UUID;
 
 @Embeddable
 public record LineId(UUID id) implements Identifier {
+
+    public static LineId of(UUID id) {
+        return new LineId(id);
+    }
+
+    @Override
+    public String toString() {
+        return id.toString();
+    }
 }
